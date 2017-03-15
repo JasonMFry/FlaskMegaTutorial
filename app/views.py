ce573dcfc4872545bafe01aa188acba093b9cd9d
Jason Fry <jason@feathr.co>
2017-03-15 15:12:49 -0400

    Add user last seen, and edit user info in profile

--- app/views.py
+++ app/views.py
@@ -1,18 +1,24 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
-from app import app, db
+from app import app, db, lm
 from .models import User
 from oauth import OAuthSignIn
+from datetime import datetime
+from .forms import LoginForm, EditForm
 
 
-# @lm.user_loader
-# def load_user(id):
-#     return User.query.get(int(id))
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 
 @app.route('/')
@@ -20,23 +26,23 @@ def before_request():
 @login_required
 def index():
     user = g.user
-    posts = [
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html',
+    # posts = [
+    #     {
+    #         'author': {'nickname': g.user.nickname},
+    #         'body': 'Beautiful day in Portland!'
+    #     },
+    #     {
+    #         'author': {'nickname': g.user.nickname},
+    #         'body': 'The Avengers movie was so cool!'
+    #     }
+    # ]
+    return render_template('user.html',
                            title='Home',
-                           user=user,
-                           posts=posts)
+                           user=user)#,
+                           # posts=posts)
 
 
-@app.route('/login', methods=['GET'])
+@app.route('/login')
 def login():
     return render_template('login.html', title='Sign In')
 
@@ -64,7 +70,45 @@ def oauth_callback(provider):
     login_user(user, True)
     return redirect(url_for('index'))
 
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm(g.user.nickname)
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
