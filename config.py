555c6e383280c845b803131834b370b23de11d22
Jason Fry <jason@feathr.co>
2017-03-15 15:14:26 -0400

    Remove OpenID functionality, add mail server ability

--- config.py
+++ config.py
@@ -15,12 +15,14 @@ OAUTH_CREDENTIALS = { #tutorial has app.config['OAUTH_CRED...']' but that's unne
     }
 }
 
-OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
-
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 2525
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['jmphry@gmail.com']
