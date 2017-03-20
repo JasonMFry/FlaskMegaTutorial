a11be9c3e59e43a88dacb2fa7475df18155e87ea
Jason Fry <jason@feathr.co>
2017-03-20 09:54:23 -0400

    Change time format to be inclusive of international locales

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 from flask_mail import Mail
+from .momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,6 +13,7 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 mail = Mail(app)
+app.jinja_env.globals['momentjs'] = momentjs
 
 if not app.debug:
 	import logging
